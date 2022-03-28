c73f74423fb8fc45f3f33e204f60b7bffcba172d
Stefan Rotaru <stefanrotaru@hotmail.com>
2022-03-28 11:14:08 +0300

    fixed indentation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Card = ({ name, email, id }) => {
-	return (
-		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/${id}?set=set3`} />
-			<div className="link b black hover-green">
-				<h2>{name}</h2>
-				<p>{email}</p>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?set=set3`} />
+      <div className="link b black hover-green">
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/robots.js
+++ src/robots.js
@@ -1,80 +1,80 @@
 export const robots = [
-{
-	id: 1,
-	name: 'Rob Bott' ,
-	username: 'Rob-o' ,
-	email: 'Robbott@robomail.biz'
-},
-{
-	id: 2,
-	name: 'Andy Roid' ,
-	username: 'Rory' ,
-	email: 'roid3000@hazzaa.net'
-},
-{
-	id: 3,
-	name: 'Sterling Bitt' ,
-	username: 'Stir' ,
-	email: 'sterlingo@bitt.com'
-},
-{
-	id: 4,
-	name: 'Cydia Cyberg' ,
-	username: 'Cycy' ,
-	email: 'Cycyberg@buzz.biz'
-},
-{
-	id: 5,
-	name: 'Spencer Prime' ,
-	username: 'Primo' ,
-	email: 'spen_primer10@robotbizz.tv'
-},
-{
-	id: 6,
-	name: 'Norbit Ofaxtron' ,
-	username: 'Norfax' ,
-	email: 'norfax01101@buzz.space'
-},
-{
-	id: 7,
-	name: 'Andromeda Prime' ,
-	username: 'The_one' ,
-	email: 'A_the_prime@one.net'
-},
-{
-	id: 8,
-	name: 'Dusty McKenzie' ,
-	username: 'Frenzy' ,
-	email: 'dustie_00101@robostuff.stuff'
-},
-{
-	id: 9,
-	name: 'Cybel Eldroid' ,
-	username: 'Cy' ,
-	email: 'eldoid55@april.bizz'
-},
-{
-	id: 10,
-	name: 'Robbie Topwires' ,
-	username: 'Best wires' ,
-	email: 'Igotswires@wires.com'
-},
-{
-	id: 11,
-	name: 'Oto Fixx' ,
-	username: 'The Fixer' ,
-	email: 'Ifixstuff@hazzaa.net'
-},
-{
-	id: 12,
-	name: 'Meg Hertz' ,
-	username: 'Charmz' ,
-	email: 'meg_hertz@cybermail.org'
-},
-{
-	id: 13,
-	name: 'Tink Ufestron' ,
-	username: 'Tinker' ,
-	email: 'the_tink@tikz.space'
-},
-]
+  {
+    id: 1,
+    name: "Rob Bott",
+    username: "Rob-o",
+    email: "Robbott@robomail.biz",
+  },
+  {
+    id: 2,
+    name: "Andy Roid",
+    username: "Rory",
+    email: "roid3000@hazzaa.net",
+  },
+  {
+    id: 3,
+    name: "Sterling Bitt",
+    username: "Stir",
+    email: "sterlingo@bitt.com",
+  },
+  {
+    id: 4,
+    name: "Cydia Cyberg",
+    username: "Cycy",
+    email: "Cycyberg@buzz.biz",
+  },
+  {
+    id: 5,
+    name: "Spencer Prime",
+    username: "Primo",
+    email: "spen_primer10@robotbizz.tv",
+  },
+  {
+    id: 6,
+    name: "Norbit Ofaxtron",
+    username: "Norfax",
+    email: "norfax01101@buzz.space",
+  },
+  {
+    id: 7,
+    name: "Andromeda Prime",
+    username: "The_one",
+    email: "A_the_prime@one.net",
+  },
+  {
+    id: 8,
+    name: "Dusty McKenzie",
+    username: "Frenzy",
+    email: "dustie_00101@robostuff.stuff",
+  },
+  {
+    id: 9,
+    name: "Cybel Eldroid",
+    username: "Cy",
+    email: "eldoid55@april.bizz",
+  },
+  {
+    id: 10,
+    name: "Robbie Topwires",
+    username: "Best wires",
+    email: "Igotswires@wires.com",
+  },
+  {
+    id: 11,
+    name: "Oto Fixx",
+    username: "The Fixer",
+    email: "Ifixstuff@hazzaa.net",
+  },
+  {
+    id: 12,
+    name: "Meg Hertz",
+    username: "Charmz",
+    email: "meg_hertz@cybermail.org",
+  },
+  {
+    id: 13,
+    name: "Tink Ufestron",
+    username: "Tinker",
+    email: "the_tink@tikz.space",
+  },
+];
